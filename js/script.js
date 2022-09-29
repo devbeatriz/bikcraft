a0454394b266d23a9a22d6940771296f781d6e15
Beatriz Rodrigues <beatrizrodrrigues@gmail.com>
2022-09-29 12:25:08 -0300

    parametros URL

--- js/script.js
+++ js/script.js
@@ -10,3 +10,16 @@ function ativarLink(link) {
 }
 
 links.forEach(ativarLink);
+
+// Ativar Items do Orçamento
+
+const parametros = new URLSearchParams(location.search);
+
+function ativarProduto(parametro) {
+  const elemento = document.getElementById(parametro);
+  if (elemento) {
+    elemento.checked = true;
+  }
+}
+
+parametros.forEach(ativarProduto);
